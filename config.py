03e5a75476127c71385587ed84154836913eacc5
Cam Ngoan <buithicamngoan@gmail.com>
2026-04-30 21:05:56 +0700

    config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-DATA_PATH = r"D:\CHATBOT_RAG\data-2\luat_hon_nhan"
+DATA_PATH =  r"/content/drive/MyDrive/CHAT_RAG_LUAT_HON_NHAN/data/luat_hon_nhan"
 
 CHUNK_SIZE = 2100
 CHUNK_OVERLAP = 100
 
 K_BM25 = 3
 K_FAISS = 10
-K_RERANK = 5
+K_RERANK = 6
 
 EMBED_MODEL = "BAAI/bge-m3"
 RERANK_MODEL = "BAAI/bge-reranker-v2-m3"
 LLM_MODEL = "qwen3"
 
-FAISS_PATH = "faiss_store"
-CACHE_PATH = "cache.json"
+FAISS_PATH =  r"/content/drive/MyDrive/CHAT_RAG_LUAT_HON_NHAN/faiss_store"
+CACHE_PATH = r"/content/drive/MyDrive/CHAT_RAG_LUAT_HON_NHAN/cache_semantic.json"
