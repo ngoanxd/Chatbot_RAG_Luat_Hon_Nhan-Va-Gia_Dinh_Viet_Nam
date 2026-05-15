a524792d7158aa6a97a5575bce12b9b5901cddbd
huongplt <lanhuongpham0508@gmail.com>
2026-05-15 23:30:23 +0700

    Update project documentation

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-DATA_PATH =  r"/content/drive/MyDrive/CHAT_RAG_LUAT_HON_NHAN/data/luat_hon_nhan"
+DATA_PATH =  r"/content/drive/MyDrive/Chatbot_RAG_Luat_Hon_Nhan-Va-Gia_Dinh_Viet_Nam/data/luat_hon_nhan"
 
 CHUNK_SIZE = 2100
 CHUNK_OVERLAP = 100
@@ -11,5 +11,5 @@ EMBED_MODEL = "BAAI/bge-m3"
 RERANK_MODEL = "BAAI/bge-reranker-v2-m3"
 LLM_MODEL = "qwen3"
 
-FAISS_PATH =  r"/content/drive/MyDrive/CHAT_RAG_LUAT_HON_NHAN/faiss_store"
-CACHE_PATH = r"/content/drive/MyDrive/CHAT_RAG_LUAT_HON_NHAN/cache/cache_semantic.json"
+FAISS_PATH =  r"/content/drive/MyDrive/Chatbot_RAG_Luat_Hon_Nhan-Va-Gia_Dinh_Viet_Nam/faiss_store"
+CACHE_PATH = r"/content/drive/MyDrive/Chatbot_RAG_Luat_Hon_Nhan-Va-Gia_Dinh_Viet_Nam/cache/cache_semantic.json"
